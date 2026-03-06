a3fef732adc824f0a0c950a31032248c397f1c53
Salafy1710 <salafy171009@student.abudzar.sch.id>
2026-03-06 08:41:25 +0700

    styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import AddStudent from "./AddStudent";
 import ListStudent from "./ListStudent";
+import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <AddStudent />
     <ListStudent />
+    <App />
   </Provider>
 );
